abcb2d5b044bd1710b361040c12d71bdc214567e
Kamil Przebieracz <dmjs23@gmail.com>
2022-04-09 18:16:13 +0200

    update

--- src/index.js
+++ src/index.js
@@ -13,18 +13,28 @@ const galleryBox = document.querySelector("div.gallery");
 
 //My API key
 const apiKey = '15302977-99a8e51ff55a3c0e02bc236e3';
-
+let pages = 1;
 //nadpisanie zachowania domyslnego form
 const submitForm = (ev) => {
-    const $form = ev.currentTarget;
-    ev.preventDefault();
+  const $form = ev.currentTarget;
+  ev.preventDefault();
+  
+  cleanGalery();
+  // pages = 1;
 
-    const { elements: {searchQuery}
-    } = $form;
+  const { elements: { searchQuery }
+  } = $form;
 
   getFoto(searchQuery.value);
-    $form.reset();
-}
+  // $form.reset();
+};
+
+function cleanGalery() {
+while (galleryBox.firstChild) {
+    galleryBox.removeChild(galleryBox.firstChild);
+  };
+  pages = 1;
+};
 
 function galleryItemsAdd(galleryItems) {
   console.log("tworzenie galeri");
@@ -46,9 +56,10 @@ function galleryItemsAdd(galleryItems) {
 //Fetch image from http
 async function getFoto(word) {
   try {
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=${word}&image_type=photo`;
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${word}&image_type=photo&per_page=40&page=${pages}`;
     console.log(url);
     const response = await axios.get(url);
+    
     const fotoArray = response.data.hits;
     console.log(fotoArray);
     galleryItemsAdd(fotoArray);
@@ -72,3 +83,16 @@ galleryBox.onclick = (ev) => {
     captionDelay: 250,
   });
 };
+
+window.addEventListener(
+  "scroll",
+  () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      console.log($form.searchQuery.value);
+      pages += 1;
+      getFoto($form.searchQuery.value);
+    };
+  }
+)
